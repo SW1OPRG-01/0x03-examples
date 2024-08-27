6e12d408b6ea136738b314320bee17dd3b83bf1f
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-08-27 21:21:02 +0200

    Add more stuff

--- src/background_check/background_check_1.cpp
+++ src/background_check/background_check_1.cpp
@@ -8,16 +8,16 @@ int main(int argc, char* argv[]) {
   std::string answer;
 
   // Get user input
-  std::cout << "What is your name? ";
+  std::cout << "What is your name?";
   std::getline(std::cin, answer);
   answers.push_back(answer);
-  std::cout << "What is your name? ";
+  std::cout << "Where were you last employed?";
   std::getline(std::cin, answer);
   answers.push_back(answer);
-  std::cout << "What is your name? ";
+  std::cout << "Have you lived abroad?";
   std::getline(std::cin, answer);
   answers.push_back(answer);
-  std::cout << "What is your name? ";
+  std::cout << "Where did you go to high school?";
   std::getline(std::cin, answer);
   answers.push_back(answer);
 
