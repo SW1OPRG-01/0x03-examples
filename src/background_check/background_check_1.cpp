b4ed83ce20a66504e21bce1fc7c036fed2047091
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-08-28 19:07:30 +0200

    Remove comments

--- src/background_check/background_check_1.cpp
+++ src/background_check/background_check_1.cpp
@@ -3,11 +3,9 @@
 #include <vector>
 
 int main(int argc, char* argv[]) {
-  // Declare variables
   std::vector<std::string> answers;
   std::string answer;
 
-  // Get user input
   std::cout << "What is your name?";
   std::getline(std::cin, answer);
   answers.push_back(answer);
--- src/background_check/background_check_2.cpp
+++ src/background_check/background_check_2.cpp
@@ -10,15 +10,13 @@ void question(std::string question, std::vector<std::string>& answers) {
 } 
 
 int main(int argc, char* argv[]) {
-  // Declare variables
   std::vector<std::string> answers;
   std::string answer;
 
-  // Get user input
   question("What is your name?", answers);
   question("Where were you last employed?", answers);
   question("Have you lived abroad?", answers);
   question("Where did you go to high school?", answers);
-  
+
   return 0;
 }
