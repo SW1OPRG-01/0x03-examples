68918596181f24c8382b8de5ba727e12b0e9eb65
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-09-26 06:50:31 +0200

    Add prototype example

---
+++ src/background_check/background_check_4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void prompt(std::string, std::vector<std::string>& answers);
+
+int main(int argc, char* argv[]) {
+  std::vector<std::string> questions = {
+    "What is your name? ", 
+    "Where were you last employed? ", 
+    "Have you lived abroad? ", 
+    "Where did you go to high school? "
+  };
+  std::vector<std::string> answers;
+  std::string answer;
+  
+  for(std::string question : questions) {
+    prompt(question, answers);
+  }
+  
+  return 0;
+}
+
+void prompt(std::string question, std::vector<std::string>& answers) {
+  std::string answer;
+  std::cout << question;
+  std::getline(std::cin, answer);
+  answers.push_back(answer);
+} 
