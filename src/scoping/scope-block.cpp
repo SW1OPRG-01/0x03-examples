9c57b1bfedf85eaf4eba7e1600d97bdc1d7267fe
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2025-02-27 21:14:31 +0100

    Change string values for clarification

--- src/scoping/scope-block.cpp
+++ src/scoping/scope-block.cpp
@@ -3,17 +3,19 @@
 
 int main(int argc, char *argv[]) {
   std::string declared_in_main = "main";
-  std::cout << "main\t\t\t" << declared_in_main << std::endl;
+  std::cout << "main\t\t" << declared_in_main << std::endl << std::endl;
   
   if(true) {
     std::string declared_in_block_1 = "block 1";
-    std::cout << "block 1\t\t\t" << declared_in_block_1 << std::endl;
-    std::cout << "main (block 1)\t\t" << declared_in_main << std::endl;
+    std::cout << "block 1\t\t" << declared_in_block_1 << std::endl;
+    std::cout << "main\t\t" << declared_in_main << std::endl << std::endl;
     {
+      std::string declared_in_main = "main (from block 2)";
+      std::string declared_in_block_1 = "block 1 (from block 2)";
       std::string declared_in_block_2 = "block 2";
-      std::cout << "block 2\t\t\t" << declared_in_block_2 << std::endl;
-      std::cout << "block 1 (block 2)\t" << declared_in_block_1 << std::endl;
-      std::cout << "main (block 2)\t\t" << declared_in_main << std::endl;
+      std::cout << "block 2\t\t" << declared_in_block_2 << std::endl;
+      std::cout << "block 1\t\t" << declared_in_block_1 << std::endl;
+      std::cout << "main\t\t" << declared_in_main << std::endl;
     }
   }
 
